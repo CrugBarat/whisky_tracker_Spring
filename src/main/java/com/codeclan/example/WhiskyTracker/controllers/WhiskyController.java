274e0b148772b9d69dfb1104c4498719f0cf5313
CrugBarat <craigtbarratt@gmail.com>
2020-06-11 13:17:47 +0100

    adds JsonBackRef to models to prevent recursion. Adds findByYear emthod in WhiskeyRepo and maps corresponding route in controller. Tests - PASSED
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,10 +1,29 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/whiskies")
 public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity getWhiskies(
+            @RequestParam(name = "year", required = false) Integer year
+    ){
+        if(year !=null) {
+            return new ResponseEntity(whiskyRepository.findByYear(year), HttpStatus.OK);
+        }
+
+        return new ResponseEntity(whiskyRepository.findAll(), HttpStatus.OK);
+    }
 
 }
